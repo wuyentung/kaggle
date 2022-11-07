60804b259ccd0124e09c037b5fe2e51d1b4988b6
wuyentung <wuyentung@gmail.com>
2022-11-07 11:08:58 +0800

    feature: cv in mlflow in Aug 22.

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Forecast a whole year numbers of selling in six countries and four books.
 - **My work**: linear regression with feature engineering by week.  
 
 It seems that the baseline version made a better prediction with feature engineering in day of year.  
-## ML-Ops
+## MLops
 Predict individual product failures (in probability) of new codes with their individual lab test results.
 - kaggle competition: Tabular Playground Series - Aug 2022
 - Baseline: [Simple Logistic Regression for Good Score (0.5837)](https://www.kaggle.com/code/ryanluoli2/simple-logistic-regression-for-good-score-0-5837)
 - **My work**:
-    - Implement MLflow, trying out using ML_Ops in main.py
+    - Implement MLflow with cross-validation in main.py
     - Fix the preprocessing issue which seeing the testing dataset before preprocessing the dataset in Baseline work
         - Filling NAs: use median value grouped by product code instead of mean to avoid skewness effect in distribution.  
 
--- Tabular Playground Series - Aug 2022/main.py
+++ Tabular Playground Series - Aug 2022/main.py
@@ -2,6 +2,7 @@
 # The dataset is from [**Classifying with Logistic Regression**](https://www.kaggle.com/competitions/tabular-playground-series-aug-2022/data?select=train.csv)
 # The analyze flow is inspired by [Simple Logistic Regression for Good Score (0.5837)](https://www.kaggle.com/code/ryanluoli2/simple-logistic-regression-for-good-score-0-5837)
 # The mlflow architecture is from https://mlflow.org/docs/latest/tutorials-and-examples/tutorial.html
+# CV example: https://github.com/mlflow/mlflow/blob/master/examples/sklearn_autolog/grid_search_cv.py
 #%%
 import os
 import warnings
@@ -14,68 +15,67 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV, cross_validate
 from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
+from pprint import pprint
 
 import logging
 from constant import *
 from preprocessing import X_train, y_train, df_test
+from utils import fetch_logged_data
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 #%%
 if __name__ == "__main__":
     
+    mlflow.sklearn.autolog()
     
     ## dataset complete
     X = X_train.copy()
     y = y_train.copy()
 
-    # Cs = [10]
-    # l1_ratios = [0.5]
-    Cs = [float(sys.argv[1])] if len(sys.argv) > 1 else [10]
-    l1_ratios = [float(sys.argv[2])] if len(sys.argv) > 2 else [0.5]
+    Cs = [5, 10]
+    l1_ratios = [0, 0.5, 1]
+    # Cs = [float(sys.argv[1])] if len(sys.argv) > 1 else [10]
+    # l1_ratios = [float(sys.argv[2])] if len(sys.argv) > 2 else [0, 0.5, 1]
     
-    for c in Cs:
-        for l1_ratio in l1_ratios:
-            with mlflow.start_run():
-                lr = LogisticRegression(C=c, max_iter=500, penalty='elasticnet', l1_ratio=l1_ratio, random_state=0, solver='saga', n_jobs=4)
-                lr.fit(X, y)
-                y_pred = lr.predict_proba(df_test.drop([ID, FAILURE], axis=1))
-                scores = cross_validate(lr, X, y, scoring=[ACCURACY, RECALL, F1, ROC_AUC], cv=10)
-                
-                accuracy = np.median(scores[f'test_{ACCURACY}'])
-                recall = np.median(scores[f'test_{RECALL}'])
-                f1 = np.median(scores[f'test_{F1}'])
-                roc_auc = np.median(scores[f'test_{ROC_AUC}'])
-                
-                print("median loss of elasticnet LogisticRegression model (C=%f, l1_ratio=%f):" % (c, l1_ratio))
-                print("  %s: %s" % (ACCURACY, accuracy))
-                print("  %s: %s" % (RECALL, recall))
-                print("  %s: %s" % (F1, f1))
-                print("  %s: %s" % (ROC_AUC, roc_auc))
+    parameters = {'C': Cs, 'l1_ratio': l1_ratios}
+    
+    lr = LogisticRegression(max_iter=500, penalty='elasticnet', random_state=0, solver='saga', n_jobs=4)
+    clf = GridSearchCV(lr, parameters)
+    clf.fit(X, y)
+    run_id = mlflow.last_active_run().info.run_id
 
-                mlflow.log_param("C", c)
-                mlflow.log_param("l1_ratio", l1_ratio)
-                mlflow.log_metric(ACCURACY, accuracy)
-                mlflow.log_metric(RECALL, recall)
-                mlflow.log_metric(F1, f1)
-                mlflow.log_metric(ROC_AUC, roc_auc)
+    # show data logged in the parent run
+    print("========== parent run ==========")
+    for key, data in fetch_logged_data(run_id).items():
+        print("\n---------- logged {} ----------".format(key))
+        pprint(data)
 
-                tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    # show data logged in the child runs
+    filter_child_runs = "tags.mlflow.parentRunId = '{}'".format(run_id)
+    runs = mlflow.search_runs(filter_string=filter_child_runs)
+    param_cols = ["params.{}".format(p) for p in parameters.keys()]
+    metric_cols = ["metrics.mean_test_score"] 
+    
+    
 
-                # Model registry does not work with file store
-                if tracking_url_type_store != "file":
+    print("\n========== child runs ==========\n")
+    pd.set_option("display.max_columns", None)  # prevent truncating columns
+    print(runs[["run_id", *param_cols, *metric_cols]])
+    
+    
+    ## submission
+    # Disable autologging after model selection is complete.
+    mlflow.sklearn.autolog(disable=True)
+    lr_best = LogisticRegression(max_iter=500, penalty='elasticnet', random_state=0, solver='saga', n_jobs=4, C = clf.best_params_['C'], l1_ratio=clf.best_params_['l1_ratio'])
 
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                    mlflow.sklearn.log_model(lr, "model", registered_model_name="elasticnet LogisticRegression Aug 22 model")
-                else:
-                    mlflow.sklearn.log_model(lr, "model")
+    lr_best.fit(X_train, y_train)
+
+    y_pred = lr_best.predict_proba(df_test.drop([ID, FAILURE], axis=1))
     submission = pd.read_csv('data/sample_submission.csv')
     submission[FAILURE] = y_pred[:,1]
     submission.to_csv("submission_baseline.csv", index=False)
