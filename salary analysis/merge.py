1e7f39afc2add706084bb5ac29bfc1977b4348ab
wuyentung <wuyentung@gmail.com>
2022-11-16 17:25:02 +0800

    feat (salary): one-hot for job level, tfidf from skill_types.

--- salary analysis/merge.py
+++ salary analysis/merge.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import re
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from constant import *
 #%%
@@ -41,4 +42,24 @@ df_merge.loc[(df_merge[SALARY_ESTIMATE].str.contains('Hour')), SALARY_LOWER] = d
 df_merge.loc[(df_merge[SALARY_ESTIMATE].str.contains('Hour')), SALARY_UPPER] = df_merge.loc[(df_merge[SALARY_ESTIMATE].str.contains('Hour')), SALARY_UPPER]*40*52/1000
 #%%
 df_merge.groupby(JOB_CAT).mean([SALARY_LOWER, SALARY_UPPER])
+#%%
+df_misplace = df_merge.loc[df_merge[SALARY_UPPER].isna()].copy()
+df_merge.drop(index=df_misplace.index, inplace=True)
+#%%
+df_merge[JOB_TITLE].unique()
+#%%
+## TODO: one-hot for level of job
+## ref: https://www.kaggle.com/code/josephgutstadt/skills-for-a-data-scientist-analyst/notebook
+
+## one-hot for job level
+df_merge[SENIOR] = df_merge[JOB_TITLE].str.contains('enior')
+df_merge[MID] = df_merge[JOB_TITLE].str.contains('Mid ') | df_merge[JOB_TITLE].str.contains('Mid-') | df_merge[JOB_TITLE].str.contains(' mid ')
+df_merge[JUNIOR] = df_merge[JOB_TITLE].str.contains('unior')
+## label amount is quite less, should use KNN or just predict salary range from skills
+#%%
+skills_list = {skill for skill_set in skill_types.values() for skill in skill_set}
+#%%
+vectorizer = TfidfVectorizer(stop_words=stopward_list, vocabulary=skills_list)
+X = vectorizer.fit_transform(df_merge[JOB_DES])
+vectorizer.get_feature_names_out()
 #%%
